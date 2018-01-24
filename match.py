f11db7eceacc064e6df2972bcde7ec33764aa1ea
Mike B <mike.bennett@ed.ac.uk>
2018-01-24 15:39:55 +0000

    dont crash if the wiki page has no title (no idea why that would be the case but apparently some of them dont)

--- match.py
+++ match.py
@@ -1,7 +1,7 @@
 from redis import Redis
 r = Redis()
 
-with open("alma3.txt", "r") as f:
+with open("alma5.txt", "r") as f:
     records = f.readlines()
     for record in records:
         rec,id = record.split(" ")
@@ -10,12 +10,15 @@ with open("alma3.txt", "r") as f:
             title = r.hget("titles", wiki).strip().replace("'","").replace("_"," ")
             print "Match! ALMA: %s LoC: %s Wiki: %s Title: %s"%(rec,id.strip(),wiki,title)
 
-with open("alma-viaf3.txt", "r") as f:
+with open("alma-viaf5.txt", "r") as f:
     records = f.readlines()
     for record in records:
         rec, id = record.split(" ")
         if r.hexists("viaf2", id):
             wiki = r.hget("viaf2", id)
-            title = r.hget("titles", wiki).strip().replace("'", "").replace("_", " ")
+            try:
+                title = r.hget("titles", wiki).strip().replace("'", "").replace("_", " ")
+            except AttributeError:
+                title = "Unknown"
             print "Match! ALMA: %s VIAF: %s Wiki: %s Title: %s"%(rec,id.strip(),wiki,title)
 
