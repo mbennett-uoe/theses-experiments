086d432af1e468e7c44154181e036f6aba3f6c5b
Mike B <mike.bennett@ed.ac.uk>
2018-01-24 15:52:51 +0000

    wider use case data results#

--- match.py
+++ match.py
@@ -1,6 +1,9 @@
 from redis import Redis
 r = Redis()
 
+results = {}
+
+
 with open("alma5.txt", "r") as f:
     records = f.readlines()
     for record in records:
@@ -8,7 +11,11 @@ with open("alma5.txt", "r") as f:
         if r.hexists("loc",id):
             wiki = r.hget("loc", id)
             title = r.hget("titles", wiki).strip().replace("'","").replace("_"," ")
-            print "Match! ALMA: %s LoC: %s Wiki: %s Title: %s"%(rec,id.strip(),wiki,title)
+            if rec in results.keys():
+                results[rec]["LOC"].append(title)
+            else:
+                results[rec] = {"LOC": [title]}
+            #print "Match! ALMA: %s LoC: %s Wiki: %s Title: %s"%(rec,id.strip(),wiki,title)
 
 with open("alma-viaf5.txt", "r") as f:
     records = f.readlines()
@@ -20,5 +27,14 @@ with open("alma-viaf5.txt", "r") as f:
                 title = r.hget("titles", wiki).strip().replace("'", "").replace("_", " ")
             except AttributeError:
                 title = "Unknown"
-            print "Match! ALMA: %s VIAF: %s Wiki: %s Title: %s"%(rec,id.strip(),wiki,title)
+            if rec in results.keys():
+                if "VIAF" in results[rec].keys():
+                    results[rec]["VIAF"].append(title)
+                else:
+                    results[rec]["VIAF"] = [title]
+            else:
+                results[rec] = {"VIAF": [title]}
+            #print "Match! ALMA: %s VIAF: %s Wiki: %s Title: %s"%(rec,id.strip(),wiki,title)
 
+from pprint import pprint
+pprint results
